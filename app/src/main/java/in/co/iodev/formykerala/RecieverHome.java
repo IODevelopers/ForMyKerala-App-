60199abb75236e182b0f583dcbce0bf7ea700f29
jisjoe <jisjoemathew@gmail.com>
2018-08-19 09:37:27 +0530

    Added Receiver Activity and Body Structure

--- app/src/main/java/in/co/iodev/formykerala/RecieverHome.java
+++ app/src/main/java/in/co/iodev/formykerala/RecieverHome.java
@@ -1,19 +1,21 @@
 package in.co.iodev.formykerala;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RecieverHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reciever_home);
     }
 
-    public void receiver(View view) {
-        startActivity(new Intent(MainActivity.this,RecieverHome.class));
+    public void request(View view) {
+
+    }
+
+    public void view_items(View view) {
     }
 }
