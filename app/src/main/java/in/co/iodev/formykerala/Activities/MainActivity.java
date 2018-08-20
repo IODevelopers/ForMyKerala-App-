facfd98359b711a667aa197c2bb7e2d0ba0e70c6
jisjoe <jisjoemathew@gmail.com>
2018-08-20 14:58:22 +0530

    MainActivity Login Check

--- app/src/main/java/in/co/iodev/formykerala/Activities/MainActivity.java
+++ app/src/main/java/in/co/iodev/formykerala/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package in.co.iodev.formykerala.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,12 +9,28 @@ import android.widget.Button;
 
 import in.co.iodev.formykerala.R;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+import static java.lang.Boolean.FALSE;
+
 public class MainActivity extends AppCompatActivity {
 Button receiver,donor;
+SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPref=getDefaultSharedPreferences(getApplicationContext());
+
+        if(sharedPref.getBoolean("Login",FALSE
+        ))
+        {
+            if(sharedPref.getBoolean("Edited",FALSE))
+                startActivity(new Intent(getApplicationContext(),ReceiverRequirementsStatus.class)); //TO VIEW ADDED REQUESTS
+            else
+                startActivity(new Intent(getApplicationContext(),ReceiverSelectRequirement.class)); //TO VIEW ADDED REQUESTS
+
+
+        }
         receiver=findViewById(R.id.role_receiver);
         donor=findViewById(R.id.role_Donor);
         receiver.setOnClickListener(new View.OnClickListener() {
